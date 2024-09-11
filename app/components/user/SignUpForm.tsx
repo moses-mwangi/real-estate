6b0d530832f1249dfa28eba23bcd9647393fd4f0
moses mwangi <moses.mwangi.me@gmail.com>
2024-09-11 12:41:58 +0300

    commit

--- app/components/user/SignUpForm.tsx
+++ app/components/user/SignUpForm.tsx
@@ -84,7 +84,7 @@ export default function SignUpForm() {
   return (
     <div>
       {show === false && (
-        <div className="pr-2 md:mr-12">
+        <div className="pr-2 mb-2 md:mr-12">
           <Card
             className="w-fit shadow-[4px_4px_0_orange]  translate-x-[3px] translate-y-[3px]  bg-card hover:shadow-lg cursor-pointer font-semibold text-gray-700 hover:text-orange-500 transition-all duration-150 text-sm py-2 px-3 rounded-md"
             onClick={() => {
