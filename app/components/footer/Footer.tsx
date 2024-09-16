7f93f920bf6b9362309368bd846e6c106e23c9a8
moses mwangi <moses.mwangi.me@gmail.com>
2024-09-16 10:54:05 +0300

    commit

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className=" bg-orange-500 py-5 px-2 sm:px-5 text-slate-50 flex flex-col sm:flex-row items-center justify-between">
-      <p>© 2024 Boma Synergy Ltd - House for sale</p>
+      <p>© 2024 Bomac Ltd Company - House for sale</p>
       <div className="flex gap-3">
         <Button className="cursor-pointer bg-orange-500 hover:bg-orange-800 py-1 rounded-sm transition-all duration-200">
           Terms Use
